828639a566d4892a4d76b8cec2c01aaff1a0f85b
Bianca <mbiancaadr@gmail.com>
2023-12-15 07:57:37 +0200

    files

--- src/components/filters/ResetButton.jsx
+++ src/components/filters/ResetButton.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { resetState, setInitialState } from "../../store/slices/movieSlice";
 import { selectInitialState } from "../../store/selectors/movieSelectors";
@@ -6,6 +7,7 @@ export default function ResetButton() {
 
     const dispatch = useDispatch();
     const initialState = useSelector(selectInitialState);
+    const [buttonClass, setButtonClass] = useState('inactive');
 
     const resetOptions = () => {
         if (initialState) return;
@@ -13,8 +15,17 @@ export default function ResetButton() {
         dispatch(resetState());
     }
 
+    useEffect(() => {
+        if (initialState) {
+            setButtonClass('inactive');
+            return;
+        }
+
+        setButtonClass('active');
+    }, [initialState]);
+
     return (
-        <div className="FilterButtonContainer Reset">
+        <div className={`FilterButtonContainer ${buttonClass} Reset`}>
             <div
                 className={`FilterButton`}
                 onClick={resetOptions}>
--- src/components/movie/FavoriteButton.jsx
+++ src/components/movie/FavoriteButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleFavorite } from "../../store/slices/movieSlice";
 import {
@@ -13,18 +14,38 @@ export default function FavoriteButton(props) {
 
     const { movieId } = props;
     const dispatch = useDispatch();
+
     const isFavorite = useSelector((state) => selectIsFavorite(state, movieId));
     const movieReleaseDate = useSelector((state) => selectMovieReleaseDate(state, movieId));
     const movieTitle = useSelector((state) => selectMovieTitle(state, movieId));
     const posterPath = useSelector((state) => selectMoviePoster(state, movieId));
 
+    const [isPulseAnimation, setIsPulseAnimation] = useState(false);
+
     const handleToggleFavorite = () => {
-        dispatch(toggleFavorite({ movieId: movieId, movieTitle: movieTitle, movieReleaseDate: movieReleaseDate, posterPath: posterPath, isFavorite: !isFavorite }));
+        dispatch(toggleFavorite({
+            movieId: movieId,
+            movieTitle: movieTitle,
+            movieReleaseDate: movieReleaseDate,
+            posterPath: posterPath,
+            isFavorite: !isFavorite
+        }));
+
+        setIsPulseAnimation(true);
+        setTimeout(() => {
+            setIsPulseAnimation(false);
+        }, 1000);
     };
 
     return (
-        <div className="FavoriteMovieButton" onClick={handleToggleFavorite}>
-            <img src={isFavorite ? fullHeartImage : emptyHeartImage} alt="heart icon" />
+        <div
+            className={`FavoriteMovieButton ${isPulseAnimation ? 'active' : ''} ${isFavorite ? 'isFavorite' : ''}`}
+            onClick={handleToggleFavorite}
+        >
+            <img
+                className="HeartIcon"
+                src={isFavorite ? fullHeartImage : emptyHeartImage}
+                alt="heart icon" />
         </div>
     );
 
